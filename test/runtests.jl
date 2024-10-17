3fe8fc663cc6521311f4b750f4dfb413609a2d38
Adrian Borucki <ab@synthillect.ai>
2024-10-17 16:21:05 +0200

    Add expermiment no. 2 as a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,37 @@ using NACE
 using Test
 
 @testset "NACE.jl" begin
-    # Write your tests here.
+    env = NACE.gym.make("MiniGrid-LavaCrossingS11N5-v0", render_mode="human")
+
+    """
+        Actions available in minigrid environments
+        """
+    IDX_TO_ACTION = Dict(
+        0 => "Turn left",
+        1 => "Turn right",
+        2 => "Move forward",
+        3 => "Unused",
+        4 => "Unused",
+        5 => "Unused",
+        6 => "Unused",
+    )
+    ACTION_TO_IDX = Dict(value => key for (key, value) ∈ IDX_TO_ACTION)
+
+    """
+            run_example(env)
+
+        Run an example on an environment.
+        """
+    function run_example(env)
+        obs, info = env.reset()
+        agent = NaceAgent(init_state(), nace_policy, nace_perceptor, nace_effector)
+        for _ ∈ 1:10
+            action = agent(obs)
+            println("Action: $(IDX_TO_ACTION[action])")
+            obs, info = env.step(action)
+            println("Current rules: $(agent.state.rules)")
+        end
+    end
+
+    run_example(env)
 end
